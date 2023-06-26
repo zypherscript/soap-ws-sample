1943e74eb153039120350330cf2446a991531de8
zypherscript <kitti.kthp@gmail.com>
2023-06-27 03:36:29 +0700

    init project

---
+++ src/main/java/com/example/soapwssample/SoapWsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.soapwssample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapWsSampleApplication.class, args);
+	}
+
+}
